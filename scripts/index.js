2755291279dc73d2b67541aafc4e70745973d79d
Hossein Hosseini <hosseinh1075@gmail.com>
2025-07-15 18:52:27 +0430

    nav

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,16 @@ import { headerTop } from "../components/header/header-top.js"
 
 window.customElements.define('header-top', headerTop)
 window.customElements.define('footer-user', footerUser)
+
+//  nav
+document.addEventListener('DOMContentLoaded', function() {
+    const nav = document.querySelector('nav.nav_item');
+    
+    window.addEventListener('scroll', function() {
+      if (window.scrollY > 40) {
+        nav.classList.add('nav_item-scroll');
+      } else {
+        nav.classList.remove('nav_item-scroll');
+      }
+    });
+  });
