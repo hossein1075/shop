0b658fc127cacb835ebb14b8252534ec3d1bf670
Hossein Hosseini <hosseinh1075@gmail.com>
2025-07-22 12:57:51 +0430

    complete page log in

--- scripts/log-in.js
+++ scripts/log-in.js
@@ -1,1 +1,19 @@
 //  button log in
+window.addEventListener('DOMContentLoaded', ()=> {
+    const logButton = document.querySelector('.log_button');
+    //  mobile
+      logButton.addEventListener('click', ()=> {
+        logButton.classList.add('btn_view--hover')
+  
+        setTimeout(()=> {
+          logButton.classList.remove('btn_view--hover')
+        }, 1000)
+      })
+      logButton.addEventListener('mouseenter', ()=> {
+        logButton.classList.add('btn_view--hover')
+      })
+      logButton.addEventListener('mouseleave', ()=> {
+        logButton.classList.remove('btn_view--hover')
+      })
+    
+  })
